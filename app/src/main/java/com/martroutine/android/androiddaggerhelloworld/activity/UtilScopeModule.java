b546739e4f4e93af13d6a91f1748fbd02fed10c7
Vtanathip <tanathip.se@gmail.com>
2014-03-19 07:25:48 +0700

    optimize import

--- app/src/main/java/com/martroutine/android/androiddaggerhelloworld/activity/UtilScopeModule.java
+++ app/src/main/java/com/martroutine/android/androiddaggerhelloworld/activity/UtilScopeModule.java
@@ -1,12 +1,8 @@
 package com.martroutine.android.androiddaggerhelloworld.activity;
 
 import com.martroutine.android.androiddaggerhelloworld.app.AndroidDaggerScopeModule;
-import com.martroutine.android.androiddaggerhelloworld.util.DateUtil;
-
-import javax.inject.Singleton;
 
 import dagger.Module;
-import dagger.Provides;
 
 @Module(
         complete = false,
